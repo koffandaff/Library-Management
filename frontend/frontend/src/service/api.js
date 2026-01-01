319b737a3e417b5063d6ee724798b6b679d1ce65
koffandaff <koffandaff@gmail.com>
2026-01-01 14:43:40 +0530

    changrs in frontend

--- frontend/frontend/src/service/api.js
+++ frontend/frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000/api';
+const baseUrl = 'https://library-management-1-61wy.onrender.com/api';
 
 const api = axios.create({
     baseURL: baseUrl,
